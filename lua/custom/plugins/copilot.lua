05a6cc095fcf7a86786154c91d5f24838ae3714d
Bob Parsons <deadstylebp@gmail.com>
2024-02-11 10:40:43 -0600

    init

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return   { "github/copilot.vim", 
+  config = function()
+    require("github/copilot.vim").setup {
+      -- Your nvim-tree configuration options go here
+    }
+
+    -- Setting up the key mapping for nvim-tree
+  end
+
+}
---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {
+      -- Your nvim-tree configuration options go here
+    }
+
+    -- Setting up the key mapping for nvim-tree
+    vim.keymap.set('n', '<leader>et', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>ec', ':NvimTreeClose', { noremap = true, silien = true })
+    vim.keymap.set('n', '<leader>eo', ':NvimTreeOpen<CR>', { noremap = true, silien = true })
+  end,
+}
