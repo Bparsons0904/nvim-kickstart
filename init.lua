104c7fd9448225774712f21644a9857b3d62fdca
Bob Parsons <deadstylep@gmail.com>
2024-02-14 21:42:49 -0600

    add github themes

--- init.lua
+++ init.lua
@@ -726,7 +726,8 @@ cmp.setup {
   },
 }
 
-vim.cmd('colorscheme kanagawa-wave')
+-- vim.cmd('colorscheme kanagawa-wave')
+vim.cmd('colorscheme bluloco')
 require('custom.config')
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
